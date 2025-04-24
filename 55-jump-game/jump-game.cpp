02bd82a3fd56e4f6af25e980947227c06568ff66
Kalpak Kawadgave <62742688+Kalpak-Kawadgave@users.noreply.github.com>
2025-04-25 02:10:51 +0530

    Time: 2845 ms (5.01%) | Memory: 53 MB (18.27%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        size_t s = nums.size();
+        vector<bool> ans(s, false);
+        ans[0] = true;
+        for (size_t i = 0; i < s-1; ++i) {
+            if (nums[i] > s - i && nums[i] == true) {
+                return true;
+            }
+            else if (ans[i] == true) {
+                for (size_t j = 0; j <= nums[i]; ++j) {
+                    if (j+i < s && !(nums[j+i]==0 && j+i<s-1) && ans[j + i] == false && ans[i]==true){
+                        ans[j + i] = ans[i];
+                    }
+                }
+            }
+            cout << ans[i] << " ";
+        }
+        return ans[s - 1];
+    }
+};
